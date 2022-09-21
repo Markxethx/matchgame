b51c0cda058b30805cae724ba4b660a790c838ba
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-09-21 22:46:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -80,12 +80,118 @@ const images = [
       number_of_items: 8,
     },
 ];
+const timeDelay = 3000;
+let currentImageValue = 0,
+ displayNumber = 0,
+ score = 0,
+ totalAvailable = images.length,
+ chosen = false;
 
-const randomNumber = Math.floor(Math.random() * images.length);
-const randomImageName = images[randomNumber].image_name;
-const imageContainer = document.getElementById('imageContainer');
+ document.getElementById('statsContent').style.visibility = "hidden";
+ document.getElementById('currentScore').innerHTML = score;
+ document.getElementById('totalAvailable').innerHTML = totalAvailable;
+ document.getElementById('timeSetting').innerHTML = timeDelay / 1000 ;
+const setImageSrc = (randomImageName) => {
+  const imageContainer = document.getElementById('imageContainer');
 
-const image = document.createElement("img");
-image.src = `images/&{randomImageName}`;
-console.log(images.src)
-imageContainer.appendChild(image);
+  if (imageContainer.hasChildNodes){
+    imageContainer.removeChild(imageContainer.firstElementChild);
+  }
+  
+  const image = document.createElement("img");
+  image.src = `images/${randomImageName}`;
+  image.classList.add("fade");
+  imageContainer.appendChild(image);
+
+}
+
+generateDisplayNumber = (numberOfItems, plusOrMinus) => {
+  split = Math.floor(Math.random() * 2)
+  if (split === 0) {
+    document.getElementById('number').innerHTML = numberOfItems;
+    displayNumber = numberOfItems
+  } else {
+    document.getElementById('number').innerHTML = `${numberOfItems + plusOrMinus}`
+    displayNumber = numberOfItems + plusOrMinus;
+  }
+
+  currentImageValue = numberOfItems;
+
+}
+
+const generatePlusOrMinus = () => {
+  const number0to1 =  Math.floor(Math.random() * 2)
+  return number0to1 === 0 ? -1 : +1;
+}
+
+const setImageName = (randomImageName) => {
+  const imageName = randomImageName.slice(0, randomImageName.length - 4);
+  document.getElementById('item-name').innerHTML = imageName
+}
+
+
+const generate = () => {
+
+  if (images.length=== 0) {
+    endOfGame();
+    stopTimer();
+    return;
+  }
+  chosen = false;
+  const randomNumber = Math.floor(Math.random() * images.length);
+  const randomImageName = images[randomNumber].image_name;
+  setImageSrc(randomImageName);
+  setImageName(randomImageName);
+  const plusOrMinus = generatePlusOrMinus();
+  const numberOfItems = images[randomNumber].number_of_items;
+  generateDisplayNumber(numberOfItems, plusOrMinus);
+
+
+  images.splice(randomNumber, 1);
+};
+
+const match = () => {
+  if (!chosen){
+    currentImageValue === displayNumber ? score++ : score --;
+    document.getElementById('currentScore').innerHTML = score;
+    chosen = true;
+  }
+
+}
+
+const noMatch = () => {
+if (!chosen){
+  currentImageValue !== displayNumber ? score++ : score --;
+  document.getElementById('currentScore').innerHTML = score;
+  chosen = true;
+}
+}
+
+let timerRef;
+const timer = () => {
+  timerRef = setInterval(generate, timeDelay);
+};
+
+const play = () => {
+  document.getElementById('message').style.display ='none';
+  document.getElementById('startScreen').style.display ='none';
+  document.getElementById('play-button').style.display ='none';
+  document.getElementById('statsContent').style.display ='block';
+  document.getElementById('statsContent').style.visibility = "visible"
+  generate();
+  timer();
+}
+
+const endOfGame = () => {
+  document.getElementById('statsContent').style.visibility = "hidden"
+  document.getElementById('message').style.display ='block';
+  document.getElementById('imageContainer').style.display ='none';
+  document.getElementById('statsContent').style.display ='none';
+  document.getElementById('message').innerHTML = `Game over your score was ${score} / ${totalAvailable}`;
+  setTimeout(() => location.reload(), 3000)
+}
+
+const stopTimer = () => {
+  clearInterval(timerRef)
+}
+
